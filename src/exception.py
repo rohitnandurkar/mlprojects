2034a83eaaeca48cffbd0214d2e6c39d2cc32e47
Rohit Nandurkar <rohitnandurkar@Rohits-MacBook-Air.local>
2025-06-17 01:34:54 -0700

    data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -2,16 +2,15 @@ import sys
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
-    , , exc_tb=error_detail.exc_info()
+    _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error+message="Error occured in python Script name [{0}] line number [{1}] error message[{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error))
-
-        return error_message
+    error_message="Error occured in python Script name [{0}] line number [{1}] error message[{2}]".format(file_name,exc_tb.tb_lineno,str(error))
+        
+    return error_message
     
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_deail:sys):
+    def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
