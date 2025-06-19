552d07a75412b840722cd726caeae6833b6016c2
Rohit Nandurkar <rohitnandurkar@Rohits-MacBook-Air.local>
2025-06-19 08:41:03 -0700

    Deployment Configuration aws

--- application.py
+++ application.py
@@ -34,6 +34,6 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True,port=5000)
+    app.run(host="0.0.0.0")
 
 
